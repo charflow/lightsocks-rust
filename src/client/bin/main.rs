5a3dcfc86e09e8519407678d6104eed8a2dda76d
Wendell Sun <iwendellsun@gmail.com>
2021-07-23 16:09:15 +0800

    refactor load config

--- src/client/bin/main.rs
+++ src/client/bin/main.rs
@@ -1,7 +1,6 @@
 #![deny(unused_imports)]
 use common::*;
 use serde_json;
-#[macro_use]
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
@@ -11,19 +10,14 @@ use tokio_socks::{tcp::Socks5Stream, Error};
 #[tokio::main]
 pub async fn main() {
     println!("client for lightsocks: version {}", VERSION);
-    if !exist_file(CONFIG_DEFAULT_PATH) {
-        dump_default_config(CONFIG_DEFAULT_PATH).unwrap();
-        println!("{} does not exist, generated.", CONFIG_DEFAULT_PATH);
-    }
-    let cfg = load_config(CONFIG_DEFAULT_PATH);
+    let cfg = load_config().unwrap();
     println!("config: {}", serde_json::to_string_pretty(&cfg).unwrap());
 
     // start proxy
     send_to_server(&cfg, 0).await.unwrap();
 }
 
-async fn send_to_server(cfg: &Config, req: TODO) -> Result<(), Error> {
-    const PROXY_ADDR: [&str; 1] = ["127.0.0.1:7890"];
+async fn send_to_server(cfg: &Config, _req: TODO) -> Result<(), Error> {
     const DEST_ADDR: &str = "google.com:80";
 
     let tcp_stream = TcpStream::connect(&cfg.remote).await?;
--- src/common/config.rs
+++ src/common/config.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::io::prelude::*;
 use std::path::Path;
+use std::env;
 
 use serde;
 use serde_json;
@@ -8,7 +9,8 @@ use shellexpand;
 
 use super::Error;
 
-pub static CONFIG_DEFAULT_PATH: &str = "~/.lightsocks.json";
+static CONFIG_DEFAULT_PATH: &str = "~/.lightsocks.json";
+const CONFIG_ENV_KEY: &str = "LIGHT_SOCKS_CONFIG";
 static CONFIG_DEFAULT: &str = r#"{
     "listen": ":7448",
     "remote": "",
@@ -23,29 +25,36 @@ pub struct Config {
     pub password: String,
 }
 
-pub fn load_config(path: &str) -> Config {
+fn load_config_by_path(path: &str) -> Config {
     let path = shellexpand::full(path).unwrap().as_ref().to_string();
     let file = fs::File::open(Path::new(&path)).expect("open config failed");
     serde_json::from_reader(file).expect("parse config failed")
 }
 
-pub fn dump_config(path: &str, cfg: &Config) -> Result<(), Error> {
-    let path = shellexpand::full(path).unwrap().as_ref().to_string();
-    let file = fs::File::create(path).expect("open config failed");
-    serde_json::to_writer_pretty(file, cfg).unwrap();
-    Ok(())
-}
-
-pub fn dump_default_config(path: &str) -> Result<(), Error> {
-    let path = shellexpand::full(path).unwrap().as_ref().to_string();
+fn dump_default_config() -> Result<(), Error> {
+    let path = shellexpand::full(CONFIG_DEFAULT).unwrap().as_ref().to_string();
     let mut file = fs::File::create(path).expect("open config failed");
     file.write_all(CONFIG_DEFAULT.as_bytes())
         .expect("write config failed");
     Ok(())
 }
 
-pub fn exist_file(path: &str) -> bool {
+fn exist_file(path: &str) -> bool {
     let path = shellexpand::full(path).unwrap().as_ref().to_string();
     let path = Path::new(&path);
     path.is_file()
+}
+
+pub fn load_config() -> Result<Config, Error> {
+    let f = match env::var(CONFIG_ENV_KEY) {
+        Ok(v) => v,
+        Err(_) => {
+            if !exist_file(CONFIG_DEFAULT_PATH) {
+                dump_default_config().unwrap();
+            }
+            CONFIG_DEFAULT_PATH.to_string()
+        },
+    };
+
+    Ok(load_config_by_path(&f))
 }
